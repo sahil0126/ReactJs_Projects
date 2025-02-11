eebee4d1cc27b002a1cef34e840d7e597d136a2c
Sahil Rajurkar <sahilrajurkar88@gmail.com>
2025-02-11 19:06:10 +0530

    till navbar

---
+++ crud-operation/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './crudfiles/Navbar'
+import './global.css'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ crud-operation/src/crudfiles/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+    <div>
+        <span className='crud'   >crud</span>
+        <span className='crud' >operation</span>
+    </div>
+    
+    </nav>
+  )
+}
+
+export default Navbar
